cfe75ba6d86933c49244765e1d4407b09daec8ce
Hung Phan <phanhung1309@gmail.com>
2023-06-02 23:28:01 +0700

    add instruction fragment layout

--- app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
@@ -18,6 +19,10 @@ class WelcomeFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentWelcomeBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_welcome, container, false)
+
+        binding.continueButton.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionFragment())
+        }
 
         return binding.root
     }
