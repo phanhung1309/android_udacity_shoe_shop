ccaf869ff0dfa73f3ceb03cb2c5f1e92303ed0fa
Hung Phan <phanhung1309@gmail.com>
2023-06-04 16:35:37 +0700

    add item_shoe layout and map into shoe list fragment

--- app/src/main/java/com/udacity/shoestore/screens/shoe_list/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoe_list/ShoeListFragment.kt
@@ -2,23 +2,33 @@ package com.udacity.shoestore.screens.shoe_list
 
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.models.Shoe
 
 class ShoeListFragment : Fragment() {
 
     private lateinit var viewModel: ShoeListViewModel
+    private lateinit var binding: FragmentShoeListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentShoeListBinding =
+        binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
+        viewModel = ViewModelProvider(requireActivity()).get(ShoeListViewModel::class.java)
+
+        binding.shoeListViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
         setHasOptionsMenu(true)
 
@@ -42,10 +52,28 @@ class ShoeListFragment : Fragment() {
         }
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.shoeList.observe(viewLifecycleOwner, Observer { shoes ->
+            binding.shoeList.removeAllViews()
+            for (shoe in shoes) {
+                addShoe(shoe)
+            }
+        })
     }
 
+    private fun addShoe(shoe: Shoe) {
+        val view = layoutInflater.inflate(R.layout.item_shoe, null)
+        val shoeTitleTextView = view.findViewById<TextView>(R.id.item_shoe_title)
+        val shoeImageView = view.findViewById<ImageView>(R.id.item_shoe_image)
+
+        shoeTitleTextView.text = shoe.name
+
+        Glide.with(requireContext())
+            .load(shoe.images)
+            .into(shoeImageView)
+
+        binding.shoeList.addView(view)
+    }
 }
